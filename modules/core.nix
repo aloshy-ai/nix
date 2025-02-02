b83b83def74900fe6d08749de4c8faa1bb98789f
aloshy.🅰🅸 <noreply@aloshy.ai>
2025-02-02 10:34:41 +0400

    Create core.nix

---
+++ modules/core.nix
@@ -0,0 +1,39 @@
+{ pkgs, ... }: {
+  # Nixpkgs configuration
+  nixpkgs.config = {
+    allowUnfree = true;
+  };
+
+  # Nix package manager configuration
+  nix = {
+    settings = {
+      experimental-features = ["nix-command" "flakes"];
+      auto-optimise-store = true;
+      cores = 0; # Use all cores
+      max-jobs = "auto"; # Use all logical cores
+      min-free = toString (1024 * 1024 * 1024); # 1 GiB
+      max-free = toString (10 * 1024 * 1024 * 1024); # 10 GiB
+      
+      substituters = [
+        "https://mirror.sjtu.edu.cn/nix-channels/store"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+      builders-use-substitutes = true;
+    };
+
+    gc = {
+      automatic = pkgs.lib.mkDefault true;
+      options = pkgs.lib.mkDefault "--delete-older-than 1d";
+      interval = {
+        Hour = 3;
+        Minute = 15;
+      };
+    };
+
+    nrBuildUsers = 32;
+    package = pkgs.nix;
+  };
+}
