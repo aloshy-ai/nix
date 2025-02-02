b86a53b19ef10a37157c8c8a4f0154557dce0dcb
aloshy.🅰🅸 <noreply@aloshy.ai>
2025-02-02 11:57:04 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,8 +10,19 @@ if ! command -v nix >/dev/null 2>&1; then
    curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
 fi
 
-# Get current system using nixpkgs
-SYSTEM=$(nix eval --impure --raw --expr 'builtins.currentSystem')
+# Get current system using uname
+case "$(uname -s)-$(uname -m)" in
+ "Darwin-arm64") SYSTEM="aarch64-darwin" ;;
+ "Darwin-x86_64") SYSTEM="x86_64-darwin" ;;
+ "Linux-x86_64") SYSTEM="x86_64-linux" ;;
+ "Linux-aarch64") SYSTEM="aarch64-linux" ;;
+ *)
+   echo "Unsupported system: $(uname -s)-$(uname -m)"
+   exit 1
+   ;;
+esac
+
+echo "Detected system: $SYSTEM"
 
 case "$SYSTEM" in
  "aarch64-darwin" | "x86_64-darwin")
@@ -30,9 +41,5 @@ case "$SYSTEM" in
    nix shell "https://flakehub.com/f/DeterminateSystems/fh/*" \
      --extra-experimental-features "nix-command flakes" \
      -c sudo fh apply nixos "flakehub://aloshy-ai/nix/*#ethernix"
-   ;;
- *)
-   echo "Unsupported system: $SYSTEM"
-   exit 1
    ;;
 esac
