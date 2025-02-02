31bb4e3b2799310a5a990aff863db3cae2cdf8bd
aloshy.🅰🅸 <noreply@aloshy.ai>
2025-02-02 14:34:42 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,14 +15,19 @@ case "$(uname -s)-$(uname -m)" in
    ;;
 esac
 
-# Install nix
+echo "INSTALLING NIX"
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --determinate --force --no-confirm
 
-# Setup Darwin configuration
-# echo "Setting up Darwin configuration..."
-# nix-env -iA nixos.pciutils
+echo "SOURCING NIX"
+. /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+
+echo "ADDING NIX-DARWIN CHANNEL"
 sudo nix-channel --add https://github.com/LnL7/nix-darwin/archive/master.tar.gz darwin
 sudo nix-channel --update
+
+echo "INSTALLING NIX-DARWIN"
 nix run  --extra-experimental-features "nix-command flakes" nix-darwin/master#darwin-rebuild -- switch
+
+echo "APPLYING ETHERMAC CONFIG"
 nix shell --extra-experimental-features "nix-command flakes" -c fh apply nix-darwin "aloshy-ai/nix/1.0.5#darwinConfigurations.ethermac"
 nix shell --extra-experimental-features "nix-command flakes" -c fh apply nix-darwin "aloshy-ai/nix/1.0.5"
