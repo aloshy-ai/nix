77c922b0371135ae5b6b5750de8b0c38ed23eef7
aloshy.🅰🅸 <noreply@aloshy.ai>
2025-02-04 00:00:53 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,9 +19,9 @@ echo "CLONING NIX FLAKES"
 echo "${GITHUB_TOKEN:+USING AUTHENTICATED GITHUB REQUESTS}"
 export DARWIN_CONFIG_DIR=$HOME/.config/nix-darwin
 rm -rf $DARWIN_CONFIG_DIR
-nix shell nixpkgs#git ${GITHUB_TOKEN:+--option access-tokens "github.com=${GITHUB_TOKEN}"} -c git clone -q https://github.com/aloshy-ai/nix $DARWIN_CONFIG_DIR
+nix shell ${GITHUB_TOKEN:+--option access-tokens "github.com=${GITHUB_TOKEN}"} nixpkgs#git -c git clone -q https://github.com/aloshy-ai/nix $DARWIN_CONFIG_DIR
 cd $DARWIN_CONFIG_DIR
 
 echo "INSTALLING NIX-DARWIN"
 echo "${GITHUB_TOKEN:+USING AUTHENTICATED GITHUB REQUESTS}"
-nix run nix-darwin -- switch --flake .#ethermac ${GITHUB_TOKEN:+--option access-tokens "github.com=${GITHUB_TOKEN}"}
+nix run nix-darwin -- switch --flake .#ethermac --force ${GITHUB_TOKEN:+--option access-tokens "github.com=${GITHUB_TOKEN}"}
