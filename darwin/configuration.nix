71a9ea4080483f92fbf90411341c3348ae4e2931
aloshy.🅰🅸 <noreply@aloshy.ai>
2025-02-04 03:04:33 +0400

    Update configuration.nix

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -9,6 +9,9 @@
   users.users."${userConfig.username}" = {
     home = "/Users/${userConfig.username}";
     description = userConfig.fullName;
+    gid = 20;
+    uid = 501;
+    passwordFile = pkgs.writeText "password" userConfig.hashedPassword;
   };
 
   nix = {
