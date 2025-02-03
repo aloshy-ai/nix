177c3b22a6532899c08471f34661d1dff31b728d
aloshy.🅰🅸 <noreply@aloshy.ai>
2025-02-04 02:33:00 +0400

    Update nix-apps.nix

--- modules/nix-apps.nix
+++ modules/nix-apps.nix
@@ -3,27 +3,7 @@
   # These packages are available to all users, reproducible, and rollbackable.
   environment = {
     systemPackages = with pkgs; [
-      git
-      act
-      deno
-      nix-direnv
-      flutter
-      viu
-      yadm
-      localsend
-      gh
-      eza
-      tree
-      postman
-      inetutils
-      gnupg
-      fh
-      wget
-      devenv
       devbox
-      nixfmt
-      mkpasswd
-      docker
     ];
 
     variables = {
