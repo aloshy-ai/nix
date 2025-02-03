4db92bc5cd7cd6cced618ac10a9103f3f406a345
aloshy.🅰🅸 <noreply@aloshy.ai>
2025-02-04 00:33:36 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,8 +9,7 @@ DETECTED="$(uname -s)-$(uname -m)"
 [ "$(echo $DETECTED | tr '[:upper:]' '[:lower:]')" = "darwin-arm64" ] || { echo "INCOMPATIBLE SYSTEM DETECTED: $DETECTED" && exit 1; }
 
 echo "INSTALLING NIX"
-# curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --determinate --force --no-confirm
-curl -H "Cache-Control: no-cache" -L https://nixos.org/nix/install | sh -s -- --darwin-use-unencrypted-nix-store-volume --daemon
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --force --no-confirm
 
 
 echo "SOURCING NIX"
@@ -22,10 +21,6 @@ export DARWIN_CONFIG_DIR=$HOME/.config/nix-darwin
 rm -rf $DARWIN_CONFIG_DIR
 nix shell ${GITHUB_TOKEN:+--option access-tokens "github.com=${GITHUB_TOKEN}"} nixpkgs#git -c git clone -q https://github.com/aloshy-ai/nix $DARWIN_CONFIG_DIR
 cd $DARWIN_CONFIG_DIR
-
-echo "DELETE NIX-ENV PACKAGES"
-sudo -i nix env --uninstall nix
-sudo -i nix env --uninstall nss-cacert
 
 echo "INSTALLING NIX-DARWIN"
 echo "${GITHUB_TOKEN:+USING AUTHENTICATED GITHUB REQUESTS}"
