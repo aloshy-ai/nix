93bf7cb6ea69adc5322b2d4ec9a0ac67d0d22a0b
aloshy.🅰🅸 <noreply@aloshy.ai>
2025-02-04 02:31:48 +0400

    Update brew-apps.nix

--- darwin/brew-apps.nix
+++ darwin/brew-apps.nix
@@ -5,7 +5,7 @@
     onActivation = {
       autoUpdate = true;
       upgrade = true;
-      cleanup = "zap"; # Uninstalls all formulae not listed here
+      cleanup = "zap";
     };
 
     taps = [
@@ -13,34 +13,11 @@
     ];
 
     brews = [
-      "fish"
-      "wget"
-      "curl"
-      "aria2"
-      "httpie"
       "nvm"
     ];
 
     casks = [
-      "chromium"
-      "framer"
       "cursor"
-      "chatgpt"
-      "claude"
-      "webstorm"
-      "visual-studio-code"
-      "discord"
-      "iina"
-      "raycast"
-      "stats"
-      "insomnia"
-      "wireshark"
-      "microsoft-edge"
-      "slack"
-      "desktoppr"
-      "github"
-      "gimp"
-      "inkscape"
     ];
   };
 }
