0cb91acb43b50c57b844c3e4e3c419150bb1811e
aloshy.🅰🅸 <noreply@aloshy.ai>
2025-02-06 14:23:12 +0400

    Update darwin.sh

--- scripts/darwin.sh
+++ scripts/darwin.sh
@@ -48,9 +48,8 @@ if [ "${CURRENT_USERNAME}" != "${FLAKE_USERNAME}" ]; then
 PASSWORD=$(openssl rand -hex 4)  # generates 8 character password
 sudo sysadminctl -addUser "${FLAKE_USERNAME}" -password "${PASSWORD}" -admin -shell "${SHELL}" -home "${FLAKE_HOME}"
 sudo sysadminctl -autologin set -userName "${FLAKE_USERNAME}" -password "${PASSWORD}"
-[ "$IS_CI" = true ] && (echo "${FLAKE_USERNAME} ALL=(ALL) NOPASSWD: ALL" | sudo tee "/etc/sudoers.d/${FLAKE_USERNAME}" > /dev/null && sudo chmod 0440 "/etc/sudoers.d/${FLAKE_USERNAME}")
 [ "$IS_CI" = false ] && sudo sysadminctl -resetPasswordFor "${FLAKE_USERNAME}" interactive
-[ "$IS_CI" = true ] && echo "${PASSWORD}" | sudo -S -u "${FLAKE_USERNAME}" sh -c "defaults write com.apple.dock persistent-apps -array; killall Dock"
+# [ "$IS_CI" = true ] && echo "${PASSWORD}" | sudo -S -u "${FLAKE_USERNAME}" sh -c "defaults write com.apple.dock persistent-apps -array; killall Dock"
 sudo dseditgroup -o edit -a "${FLAKE_USERNAME}" -t user admin
 echo "USER CREATED SUCCESSFULLY: ${FLAKE_USERNAME}"
     elif [ "${CURRENT_HOME}" = "${FLAKE_HOME}" ]; then
