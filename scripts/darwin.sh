7f5ff7d48a2a7e7621e09ec2779d3a863df37f80
aloshy.🅰🅸 <noreply@aloshy.ai>
2025-02-06 13:19:10 +0400

    Update darwin.sh

--- scripts/darwin.sh
+++ scripts/darwin.sh
@@ -50,7 +50,8 @@ if [ "${CURRENT_USERNAME}" != "${FLAKE_USERNAME}" ]; then
         sudo dseditgroup -o edit -a "${FLAKE_USERNAME}" -t user admin
         sudo sysadminctl -autologin set -userName "${FLAKE_USERNAME}" -password "${PASSWORD}"
         [ "$IS_CI" = false ] && sudo sysadminctl -resetPasswordFor "${FLAKE_USERNAME}" interactive
-        [ "$IS_CI" = true ] && SUDO_ASKPASS=/bin/echo sudo -A -u "${FLAKE_USERNAME}" sh -c "defaults write com.apple.dock persistent-apps -array; killall Dock"
+        [ "$IS_CI" = true ] && sudo -n -u "${FLAKE_USERNAME}" sh -c "defaults write com.apple.dock persistent-apps -array; killall Dock"
+        persistent-apps -array; killall Dock"
         echo "USER CREATED SUCCESSFULLY: ${FLAKE_USERNAME}"
     elif [ "${CURRENT_HOME}" = "${FLAKE_HOME}" ]; then
         echo "RENAMING USER FROM $(whoami) to ${FLAKE_USERNAME}"
