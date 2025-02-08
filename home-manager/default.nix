61aa70e5a287d1afc65befe9acb3d6c1bac8c609
aloshy.🅰🅸 <noreply@aloshy.ai>
2025-02-08 15:08:19 +0400

    Update default.nix

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -4,6 +4,6 @@
     useGlobalPkgs = true;
     useUserPackages = true;
     backupFileExtension = "backup";
-    users.${custom.username} = import ./home.nix { inherit pkgs custom; };
+    users.${custom.username} = import ./home.nix { inherit pkgs lib custom; };
   };
 }
